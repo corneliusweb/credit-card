6ea252be83080c5a8f61ca3beffa5e60f7562b48
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-07-22 16:28:30 +0100

    Add images, pages and components files

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import bgCardBack from './bg-card-back.png';
+import bgCardFront from './bg-card-front.png';
+import bgMainMobile from './bg-main-mobile.png';
+import bgMainDesktop from './bg-main-desktop.png';
+import cardLogo from './card-logo.svg';
+import iconComplete from './icon-complete.svg';
+
+export {
+	bgCardBack,
+	bgCardFront,
+	bgMainMobile,
+	bgMainDesktop,
+	cardLogo,
+	iconComplete,
+};
