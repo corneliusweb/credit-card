25f3fb31376e1f2f2840036866bf35f3630ec3be
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-07-30 17:58:21 +0100

    Adjust error messages and render it styled

--- src/components/NewCardForm.jsx
+++ src/components/NewCardForm.jsx
@@ -5,16 +5,14 @@ import Button from './Button';
 const cardSchema = z.object({
 	cardNumber: z
 		.string()
-		.length(16, 'Card number must be exactly 16 digits')
-		.regex(/^\d+$/, 'Wrong format, numbers only.'),
+		.regex(/^\d+$/, 'Wrong format, numbers only.')
+		.length(16, 'Card number must be exactly 16 digits'),
 	cardHolder: z
 		.string()
-		.max(25, 'Name is too long')
-		.regex(/^[A-Za-z\s]+$/, 'Name must contain only letters and spaces'),
-	expiryMonth: z
-		.string()
-		.regex(/^(0[1-9]|1[0-2])$/, 'Month must be between 01 and 12'),
-	expiryYear: z.string().regex(/^\d{2}$/, 'Year must be two digits'),
+		.regex(/^[A-Za-z\s]+$/, 'Name must contain only letters and spaces')
+		.max(17, 'Name too long, please use only your firstName'),
+	expiryMonth: z.string().regex(/^(0[1-9]|1[0-2])$/, "Month can't be blank"),
+	expiryYear: z.string().regex(/^\d{2}$/, "Year can't be blank"),
 	cvv: z.string().regex(/^\d{3}$/, 'CVV must be 3 digits'),
 });
 
@@ -79,13 +77,14 @@ const NewCardForm = ({ cardStates }) => {
 						name='cardHolder'
 						placeholder='e.g Jane Appleseed'
 						required
+						maxLength={18}
 						autoFocus={true}
 						onChange={handleChange}
 						className='input-style'
 					/>
 				</div>
 				{errors.cardHolder && (
-					<p className='text-red-400 capitalize'>{errors.cardHolder}</p>
+					<p className='error-style'>{errors.cardHolder}</p>
 				)}
 			</label>
 			<label className='label-style'>
@@ -102,9 +101,16 @@ const NewCardForm = ({ cardStates }) => {
 						className='input-style'
 					/>
 				</div>
+				{errors.cardNumber && (
+					<p className='error-style'>{errors.cardNumber}</p>
+				)}
 			</label>
 			<div className='flex justify-between mb-8 gap-4'>
-				<fieldset className='flex gap-2 w-1/2'>
+				<fieldset
+					className={`flex gap-2 w-1/2 ${
+						errors.expiryMonth || errors.expiryYear ? 'relative mb-2' : ''
+					}`}
+				>
 					<legend className='uppercase text-sm font-semibold tracking-wider'>
 						Exp. Date (MM / YY)
 					</legend>
@@ -132,6 +138,13 @@ const NewCardForm = ({ cardStates }) => {
 							className='flex-input-style'
 						/>
 					</div>
+					{/* display error when either of month & year is true */}
+					{(errors.expiryMonth || errors.expiryYear) && (
+						<p className='error-style absolute -bottom-4.5'>
+							{/* first display month if true, then year after */}
+							{errors.expiryMonth || errors.expiryYear}
+						</p>
+					)}
 				</fieldset>
 
 				<label className='label-style mb-0 grow'>
@@ -148,6 +161,7 @@ const NewCardForm = ({ cardStates }) => {
 							className='flex-input-style block py-2 w-full'
 						/>
 					</div>
+					{errors.cvv && <p className='error-style'> {errors.cvv} </p>}
 				</label>
 			</div>
 			<Button type={'submit'} label={'Confirm'} />
